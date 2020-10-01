54edb32d571fdddbd5d2f0548a3ecacfb31f906b
RoblesJP <juan.robles1417@gmail.com>
2020-09-30 22:42:40 -0300

    Agregar punto2/

---
+++ punto2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Cadeteria;
+
+namespace punto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empresa miCadeteria1 = new Empresa("Mi Cadeteria");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
